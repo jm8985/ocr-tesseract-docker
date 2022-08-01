da83e5df7cffccaddd29a821ef30ff9d76e8b33a
Jacob <jacobmiller1@gmail.com>
2022-08-01 16:40:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ def upload_file():
       gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
       
       # apply thresholding to preprocess the image
-      gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+      #gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
       # apply median blurring to remove any blurring
-      gray = cv2.medianBlur(gray, 3)
+      #gray = cv2.medianBlur(gray, 3)
 
       # save the processed image in the /static/uploads directory
       ofilename = os.path.join(app.config['UPLOAD_FOLDER'],"{}.png".format(os.getpid()))
